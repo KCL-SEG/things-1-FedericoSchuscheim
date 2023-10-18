2b92b0d2a5a9e18274211699aa681bb9460c74c5
Federico Schuscheim <federico.schuscheim@kcl.ac.uk>
2023-10-18 22:04:20 +0100

    Unrefine thing model

--- things/models.py
+++ things/models.py
@@ -1,13 +1,7 @@
 from django.db import models
-from django.core.validators import MinValueValidator
-from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name=models.CharField(max_length=30, unique=True, blank=False)
-    description=models.CharField(max_length=120, unique=False, blank=True)
-    quantity=models.IntegerField(unique=False,
-                                   validators= [MinValueValidator(limit_value=0),
-                                                MaxValueValidator(limit_value=100)
-                                                ]
-                                   )
+    name=models.CharField(max_length=30)
+    description=models.CharField(max_length=120)
+    quantity=models.IntegerField(unique=False)
