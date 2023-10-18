b6079f4a435578ff5c07ad64e016738aff959218
Federico Schuscheim <federico.schuscheim@kcl.ac.uk>
2023-10-18 21:37:51 +0100

    Remove custom validator from quantity in thing model

--- things/models.py
+++ things/models.py
@@ -7,8 +7,8 @@ class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=120, unique=False, blank=True)
     quantity = models.IntegerField(unique=False,
-                                   validators= [MinValueValidator(
-                                                    limit_value=0, message='Cannot be less than 0'),
-                                                MaxValueValidator(
-                                                    limit_value=100, message='Cannot be more than 100')]
+                                   #validators= [MinValueValidator(
+                                    #                limit_value=0, message='Cannot be less than 0'),
+                                     #           MaxValueValidator(
+                                      #              limit_value=100, message='Cannot be more than 100')]
                                    )
